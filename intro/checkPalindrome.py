c64c4d9f95d3a1c019c4b7ab91af75b4428f8825
Muhammed Bahadir Kucuk <munisebahadir@gmail.com>
2020-05-19 11:27:00 +0200

    Update checkPalindrome.py

--- intro/checkPalindrome.py
+++ intro/checkPalindrome.py
@@ -1,15 +1,2 @@
 def checkPalindrome(inputString):
-    middle_index = len(inputString) // 2
-    
-    if len(inputString) == 1:
-        return True
-    elif len(inputString) % 2 == 0:
-        if inputString[0:middle_index][::-1] == inputString[middle_index:]:
-            return True
-        else:
-            return False
-    else:
-        if inputString[:middle_index][::-1] == inputString[middle_index + 1:]:
-            return True
-        else:
-            return False
+    return inputString == inputString[::-1]
